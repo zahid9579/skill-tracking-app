f425511d4342590a5de73f7f5fc64a88afbe1938
Zahid-9579 <zahidansari.er@gmail.com>
2025-03-07 23:55:51 +0530

    Database config done

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express();
 
+// const db = require('./db');
+// const jwtConfig = require('./JWT');
+
+
+
+
 const port = process.env.port || 3000;
 
 
