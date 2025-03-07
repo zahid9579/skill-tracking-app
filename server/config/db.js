850b2054166a6a13590236aa5b4042d5314f6997
Zahid-9579 <zahidansari.er@gmail.com>
2025-03-07 23:56:39 +0530

    Database config done

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose')
-mongoose.connect(""){
-    try{
-        console.log("Mongodb is connected")
+// const mongoose = require('mongoose')
+// mongoose.connect(""){
+//     try{
+//         console.log("Mongodb is connected")
 
-    }catch(err){
-        console.log("Something went wrong while connection of DB")
-    }
+//     }catch(err){
+//         console.log("Something went wrong while connection of DB")
+//     }
     
-}
+// }
